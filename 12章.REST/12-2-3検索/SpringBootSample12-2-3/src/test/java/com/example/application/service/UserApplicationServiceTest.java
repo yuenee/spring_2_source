69e734eeefcce85436d1b41d1b651642f0c0cb82
yuenee <yuenee.op@gmail.com>
2022-05-17 09:19:40 +0900

    実行確認のため、簡単なテストを実行してみる

---
+++ 12章.REST/12-2-3検索/SpringBootSample12-2-3/src/test/java/com/example/application/service/UserApplicationServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserApplicationServiceTest {
+    @Test
+    public void test() {
+        assertEquals(2 ,1 + 1);
+    }
+}
